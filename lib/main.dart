156ad5dafaaa81419d40b2387fb177d5c798f5ab
aldosaucedo <a.22308051281310@cbtis128.edu.mx>
2025-05-06 16:05:03 +0000

    aldosaucedo

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:saucedorutas/pagina_uno.dart';
 import 'package:saucedorutas/pagina_stack.dart';
 import 'package:saucedorutas/pagina_raw.dart';
-import 'package:saucedorutas/pagina_cuatro.dart';
-import 'package:saucedorutas/pagina_cinco.dart';
-import 'package:saucedorutas/pagina_seis.dart';
-import 'package:saucedorutas/pagina_siete.dart';
-import 'package:saucedorutas/pagina_ocho.dart';
-import 'package:saucedorutas/pagina_nueve.dart';
+import 'package:saucedorutas/pagina_cliprrect.dart';
+import 'package:saucedorutas/pagina_switches.dart';
+import 'package:saucedorutas/pagina_wrap.dart';
+import 'package:saucedorutas/pagina_selecttable.dart';
+import 'package:saucedorutas/pagina_draggable.dart';
+import 'package:saucedorutas/pagina_cascade.dart';
 
 void main() => runApp(MirutasApp());
 
@@ -24,12 +24,12 @@ class MirutasApp extends StatelessWidget {
         '/': (context) => const Pantallauno(),
         '/pantalla2': (context) => const Pantallastack(),
         '/pantalla3': (context) => const Pantallaraw(),
-        '/pantalla4': (context) => const Pantallacuatro(),
-        '/pantalla5': (context) => const Pantallacinco(),
-        '/pantalla6': (context) => const Pantallaseis(),
-        '/pantalla7': (context) => const Pantallasiete(),
-        '/pantalla8': (context) => const Pantallaocho(),
-        '/pantalla9': (context) => const Pantallanueve(),
+        '/pantalla4': (context) => const Pantallacliprrect(),
+        '/pantalla5': (context) => const Pantallasswitches(),
+        '/pantalla6': (context) => const Pantallawrap(),
+        '/pantalla7': (context) => const Pantallaselecttable(),
+        '/pantalla8': (context) => const Pantalladraggable(),
+        '/pantalla9': (context) => const Pantallacascade(),
       },
     );
   }
